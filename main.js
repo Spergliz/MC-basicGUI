a352e870c1eb8cd0ba65d650d058908c6ef9b22b
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-12-05 14:53:55 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,63 @@
 // My Contacts Basic
-let contacts = []
+let contacts = [""];
 // HTML Elements
-let goBtnEl = document.getElementById('go-btn');
-let menuEl = document.getElementById('menu');
-let contactsE1 = document.getElementById('contacts');
+let goBtnEl = document.getElementById("go-btn");
+let menuEl = document.getElementById("menu");
+let contactsE1 = document.getElementById("Contacts");
 
 // Go Btn - Menu Listener
-goBtnEl.addEventListener('click', goBtnHandler);
+goBtnEl.addEventListener("click", goBtnHandler);
 
 function goBtnHandler() {
   // Get Menu Selection
   let selection = menuEl.value;
 
-  if (selection === 'display-all') {
+  if (selection === "display-all") {
     displayContacts();
-  } else if (selection === 'add') {
+  } else if (selection === "add") {
     addContact();
-  } else if (selection === 'remove') {
+  } else if (selection === "remove") {
     removeContact();
-  } else if (selection === 'display-name') {
+  } else if (selection === "display-name") {
     displayByName();
-  } else if (selection === 'display-country') {
+  } else if (selection === "display-country") {
     displayByCountry();
   }
 }
 
 // MENU FUNCTIONS
 function displayContacts() {
-for(let i = 0; i<contacts.length; i++) {
-  contactsE1.innerHTML = `${contacts[i]}`
+  for (let i = 0; i < contacts.length; i++) {
+    contactsE1.innerHTML = `${contacts[i].N}`;
+  }
 }
-}
-
 
 function addContact() {
-  let N = prompt("give contact name") 
-  let E = prompt("give contact Email")
-  let C = prompt("give contact country")
-  contacts.push
+  let N = prompt("give contact name");
+  let E = prompt("give contact Email");
+  let p = prompt("give contact phone Num")
+  let C = prompt("give contact country");
+  contacts.push(newcontact(N,E,p,C));
+  contactsE1.innerHTML += `contact added: ${N,E,p,C}`
 }
 
 function removeContact() {
-  console.log('Remove Contact');
+  console.log("Remove Contact");
 }
 
 function displayByName() {
-  console.log('Display by Name');
+  console.log("Display by Name");
 }
 
 function displayByCountry() {
-  console.log('Display by Country');
+  console.log("Display by Country");
+}
+
+function newcontact(name, email, phone, country) {
+  return {
+    N: name,
+    E: email,
+    p: phone,
+    C: country
+  };
 }
